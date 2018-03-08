80aaaf6c033a978d702cce75536feba5352cf35a
gjTool <hwj95@qq.com>
2018-03-08 17:42:47 +0800

    修改测试

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
 const gulp = require('gulp');
-const wrap = require('gulp-wrap');
 const uglify = require('gulp-uglify');
 const rename = require("gulp-rename");
 
 gulp.task('mini', () => (
-  gulp.src('src/src.js')
-  .pipe(wrap({src: 'src/exports.js'}))
+  gulp.src("dist/gjtool.js")
   .pipe(rename("gjtool.js"))
   .pipe(gulp.dest('dist/'))
   .pipe(uglify())    //uglify
